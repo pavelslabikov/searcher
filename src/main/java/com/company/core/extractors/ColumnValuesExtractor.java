fd264e6c9dfec28f4c6cd18778fc5304380d9dae
Pashka <pslabikov@mail.ru>
2021-02-03 18:06:36 +0500

    Added filename and column number configuring feature

--- src/main/java/com/company/core/extractors/ColumnValuesExtractor.java
+++ src/main/java/com/company/core/extractors/ColumnValuesExtractor.java
@@ -3,7 +3,9 @@ package com.company.core.extractors;
 import au.com.bytecode.opencsv.CSVReader;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.TreeMultimap;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
@@ -12,32 +14,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 @Component
-@Primary
+@Scope("prototype")
+@PropertySource("classpath:application.yml")
 public class ColumnValuesExtractor implements DataManager {
     private final Multimap<String, String> fileContent;
+    private final String filename;
     private final int columnNumber;
 
-    public ColumnValuesExtractor(int columnNumber) {
-        this.columnNumber = columnNumber;
+    public ColumnValuesExtractor(@Value("${app.filename}") String filename,
+                                 @Value("${app.column-number}") int number) {
+        this.filename = filename;
         fileContent = TreeMultimap.create();
+        if (number <= 0)
+            throw new IllegalArgumentException("Номер колонки должен быть > 0");
+
+        columnNumber = number - 1;
     }
 
     @Override
-    public String[] extractData(String filename) {
-        try {
-            var reader = new CSVReader(new FileReader("src/main/resources/airports.dat"), ',' , '"' , 1);
+    public String[] extractData() {
+        try (var reader = new CSVReader(new FileReader(filename))) {
             String[] nextLine;
             while (true) {
                 nextLine = reader.readNext();
-                if (nextLine != null) {
-                    fileContent.put(nextLine[columnNumber], Arrays.toString(nextLine).intern());
-                } else
+                if (nextLine == null)
                     break;
+
+                if (columnNumber >= nextLine.length)
+                    continue;
+
+                fileContent.put(nextLine[columnNumber], Arrays.toString(nextLine).intern());
             }
 
-            reader.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
 
         return fileContent.keySet().toArray(new String[0]);
@@ -46,8 +56,8 @@ public class ColumnValuesExtractor implements DataManager {
     @Override
     public String[] transformData(String[] data) {
         var result = new ArrayList<String>();
-        for (String datum : data)
-            result.addAll(fileContent.get(datum));
+        for (String column : data)
+            result.addAll(fileContent.get(column));
         return result.toArray(new String[0]);
     }
 }
--- src/main/java/com/company/core/extractors/DataManager.java
+++ src/main/java/com/company/core/extractors/DataManager.java
@@ -1,6 +1,6 @@
 package com.company.core.extractors;
 
 public interface DataManager {
-    String[] extractData(String filename);
+    String[] extractData();
     String[] transformData(String[] data);
 }
